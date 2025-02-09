11e174fb46df58ac4df866b4f6960941c5a53f45
Andrew Ward <wardac@vcu.edu>
2025-02-09 18:03:36 -0500

    some dark colors

--- config.def.h
+++ config.def.h
@@ -3,9 +3,9 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#002b36",     /* after initialization */
+	[INPUT] =  "#586e75",   /* during input */
+	[FAILED] = "#073642",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
